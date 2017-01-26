2e43a53cc67cb9f46dda9e43b6da99f155380874
emikucki <lockwoodlab@ip0af52ae0.int.uvm.edu>
2017-01-26 17:21:47 -0500

    updated demo1

---
+++ First_Script.R
@@ -0,0 +1,11 @@
+#' "Special" roxygen comment that will render as markdown
+#Demo of R Scripts
+#January 26, 2017
+#Emily Mikucki
+
+#New plot to show up in viewer 
+plot(runif(10))
+
+#Show some data in the console 
+z <- rnorm(n=3, mean=10, sd=2)
+print(z)
---
+++ Markdown_Demo.R
@@ -0,0 +1,13 @@
+## ------------------------------------------------------------------------
+# Annotation/comment
+Time <- seq(1,10) # seq. makes integer sequence
+print(Time)   # shows contents
+Resp <- runif(10) #random uniform (runif), gives 10 random uniforms
+print(Resp)
+plot(x=Time, y=Resp, type="b") #b is both  point and line connecting 
+
+
+## ------------------------------------------------------------------------
+plot(runif(20))
+print(seq(1,5))
+
